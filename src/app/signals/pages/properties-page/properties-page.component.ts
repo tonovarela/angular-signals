ed83837735fcd81987d2491494eb60c7e822dfe3
tonovarela <tonovarela@live.com>
2023-05-28 15:11:43 -0600

    Uso de effects en angular

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,28 +1,49 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, effect, signal } from '@angular/core';
 import { User } from '../../interfaces';
 
 @Component({
   templateUrl: './properties-page.component.html',
   styleUrls: ['./properties-page.component.css']
 })
-export class PropertiesPageComponent {
+export class PropertiesPageComponent implements OnInit, OnDestroy {
+  interval!:any;
+  ngOnInit(): void {
+     this.interval =setInterval(() => {
+      console.log("Interval");
+      this.increaseBy(1);
 
-  public user= signal<User> ({
-    id:1,
-    email:"tonovarela",
-    first_name:"Tony",
-    last_name:"Varela",
-    avatar:"https://avatars.githubusercontent.com/u/1?v=3",
+    }, 1000);
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+    //this.userChangeEffect.destroy();
+  }
 
 
+
+  public counter = signal(3);
+
+
+  public user = signal<User>({
+    id: 1,
+    email: "tonovarela",
+    first_name: "Tony",
+    last_name: "Varela",
+    avatar: "https://avatars.githubusercontent.com/u/1?v=3",
   });
+
+  public userChangeEffect = effect(() => {
+    console.log(`${this.user().first_name}-${this.counter()}`);
+  });
+
+
   onFieldUpdated(field: keyof User, value: string) {
 
 
-    this.user.update(current=>{
+    this.user.update(current => {
       return {
         ...current,
-        [field]:value
+        [field]: value
       }
     })
     // this.user.mutate(current =>{
@@ -46,11 +67,14 @@ export class PropertiesPageComponent {
     //       break;
     //   }
     // });
-  //this.user.set({...this.user(),[field]: value});
+    //this.user.set({...this.user(),[field]: value});
 
-    console.log({field, value});
+    //console.log({field, value});
   }
 
-  
+  increaseBy(value: number) {
+    this.counter.update(current => current + value)
+
+  }
 
 }
